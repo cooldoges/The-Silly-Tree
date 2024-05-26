7473ee328e4987bd9a3e944f08239b1fb84b3767
cooldoges <jaljr1287@gmail.com>
2024-05-25 19:40:24 -0500

    bean bag
    yeah

--- js/layers.js
+++ js/layers.js
@@ -13,8 +13,9 @@ addLayer("e", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+    gainMult() {
+        let mult = new Decimal(1)
+        if (hasUpgrade('e', 13)) mult = mult.times(upgradeEffect('e', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -25,6 +26,62 @@ addLayer("e", {
         {key: "e", description: "E: Reset for eggs", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
-  
+    upgrades: {
+        11: {
+            title: "Egg powered generators",
+            description: "Double your Golden Coin gain.",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "Gold coated Eggs",
+            description: "Increase Golden Coin Gain by Eggs.",
+            cost: new Decimal(3),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+        13: {
+            title: "Melted Gold",
+            description: "Increase Egg Gain by Golden Coins.",
+            cost: new Decimal(5),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+        },
+    },
 
+})
+addLayer("p", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: false,
+        requires: new Decimal(200),       // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#4BDC13",                       // The color for this layer, which affects many elements.
+    resource: "Paint Buckets",            // The name of this layer's main prestige resource.
+    row: 1,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "points",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+
+    upgrades: {
+        // Look in the upgrades docs to see what goes here!
+    },
 })
--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function getPointGen() {
 
 		let gain = new Decimal(1)
 		if (hasUpgrade('e', 11)) gain = gain.times(2)
-		if (hasUpgrade('e', 12)) gain = gain.times(upgradeEffect('e', 12))		
+		if (hasUpgrade('e', 12)) gain = gain.times(upgradeEffect('e', 12))			
 		return gain
 }
 
